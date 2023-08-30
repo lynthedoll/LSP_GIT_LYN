aa2adc96cd04bc2fb9d8977529fff0612c07edf3
lynthedoll <caitlyn.lynch@bison.howard.edu>
2023-08-30 00:59:38 -0400

    Initial commit

---
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Name:  Caitlyn Lynch 
+ * Student ID #:  @02995610
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
